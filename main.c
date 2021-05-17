43b4d88cccc73e21e262405969f6ae098b5e5475
jfrancis <jfrancis@student.42sp.org.br>
2021-05-17 13:39:28 -0300

    finaalizando

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 int main ()
 {
 	static char	a01;
+	printf("printf: |%*.3d|", 6, 5);
+	printf("\n");
+	ft_printf("ft_printf: |%*.3d|", 6, 5);
+	printf("\n");
 	printf("printf: |%*.3d|", 5, -4);
 	printf("\n");
 	ft_printf("ft_printf: |%*.3d|", 5, -4);
