f0e7813ff9a784a29bd489aea7fb5e1010c277b7
jfrancis <jfrancis@student.42sp.org.br>
2021-05-17 13:57:03 -0300

    finaalizando

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: jfrancis <jfrancis@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/06 16:46:00 by jfrancis          #+#    #+#             */
-/*   Updated: 2021/05/17 13:56:01 by jfrancis         ###   ########.fr       */
+/*   Updated: 2021/05/17 13:56:59 by jfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@ void	ft_putnbr(char *number, int n_len, t_specs *spec)
 	int output;
 	ft_putstr(number);
 	output = ft_strlen(number);
+	n_len++;
+	n_len--;
 	spec->total_chars = spec->total_chars + output;
 }
