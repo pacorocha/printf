8e27d5bc255442b48f914584621fcd718b7b9914
jfrancis <jfrancis@student.42sp.org.br>
2021-05-17 12:08:59 -0300

    final

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <ft_printf.h>
-
-void main()
-{
-printf(" --0*%0*.0u*0 0*%0*.10u*0-- ", -21, UINT_MAX, 21, UINT_MIN);
-printf("\n");
-ft_printf(" --0*%0*.0u*0 0*%0*.10u*0-- ", -21, UINT_MAX, 21, UINT_MIN);
-printf("\n");
-}
