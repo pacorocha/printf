ceb910f89c8f10485920e310d9e67e11534dec26
jfrancis <jfrancis@student.42sp.org.br>
2021-05-17 17:02:06 -0300

    finaalizando

--- get_char.c
+++ get_char.c
@@ -6,7 +6,7 @@
 /*   By: jfrancis <jfrancis@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 23:07:35 by jfrancis          #+#    #+#             */
-/*   Updated: 2021/05/17 14:37:46 by jfrancis         ###   ########.fr       */
+/*   Updated: 2021/05/17 17:01:38 by jfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	get_char(char c, va_list args, t_specs *spec)
 	else
 		k = va_arg(args, int);
 	output = spec->width;
-	if (c == '0')
+	if (c == '0' && spec->precision == 0)
 		output = spec->width - 1;
 	if (spec->lalign == 0)
 		print_fill(output, spec);
