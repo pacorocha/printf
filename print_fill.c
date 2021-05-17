85254fadf11e22b6cdb7d40cd34ffeb93953920f
jfrancis <jfrancis@student.42sp.org.br>
2021-05-17 14:07:39 -0300

    finaalizando

--- print_fill.c
+++ print_fill.c
@@ -6,7 +6,7 @@
 /*   By: jfrancis <jfrancis@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 19:40:38 by jfrancis          #+#    #+#             */
-/*   Updated: 2021/04/14 22:18:09 by jfrancis         ###   ########.fr       */
+/*   Updated: 2021/05/17 14:07:35 by jfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,6 @@ void	print_fill(int output, t_specs *spec)
 	int		i;
 
 	i = 0;
-	if (spec->filler == '0')
-		c = '0';
-	else
-		c = ' ';
 	while (output > 0)
 	{
 		ft_putchar(c);
